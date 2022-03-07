65f4cad66d2077e12e6b908034ae1200d5f7de68
hrishik <hrishikthakur1@gmail.com>
2022-03-07 11:54:26 +0530

    nothing

--- Level 1/09. Generic Tree/03. Size of a Generic Tree.java
+++
@@ -1,7 +0,0 @@
-public static int size(Node node) {
-  int count = 1;
-  for (Node child : node.children) {
-    count += size(child);
-  }
-  return count;
-}
--- Level 1/09. Generic Tree/05. Maximum in a Generic Tree.java
+++
@@ -1,7 +0,0 @@
-public static int max(Node node) {
-  int max = node.data;
-  for (Node child : node.children) {
-    max = Math.max(max, max(child));
-  }
-  return max;
-}
