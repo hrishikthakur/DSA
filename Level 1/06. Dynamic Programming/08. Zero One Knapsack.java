40b5ae3cc703f3368386676e3ca0656e89c2c316
hrishik <hrishikthakur1@gmail.com>
2022-04-07 12:21:40 +0530

    Why 2-D dp

--- Level 1/06. Dynamic Programming/08. Zero One Knapsack.java
+++ Level 1/06. Dynamic Programming/08. Zero One Knapsack.java
@@ -1,5 +1,31 @@
-// ************************************ Tabulation ************************************
+// ************************************ Memoization ************************************
 
+// Note that there are two varying variables -> idx & cap
+// Hence in tabulation a 2-D dp is required
+
+public static int zokMemo(int idx, int cap, int[] profits, int[] weights, int[][] dp) {
+    // +ve base case
+    if (idx == profits.length) return 0;
+
+    if (dp[idx][cap] != -1) return dp[idx][cap];
+
+    int yes = 0;
+    if (cap - weights[idx] >= 0) {
+        yes = profits[idx] + zokMemo(idx + 1, cap - weights[idx], profits, weights, dp);
+    }
+    int no = zokMemo(idx + 1, cap, profits, weights, dp);
+
+    dp[idx][cap] = Math.max(yes, no);
+    return dp[idx][cap];
+}
+
+// int n = weights.length;
+// int[][] dp = new int[n + 1][cap + 1];
+// for (int i = 0; i <= n; i++)
+//     Arrays.fill(dp[i], -1);
+// System.out.println(zokMemo(0, cap, profits, weights, dp));
+
+// ************************************ Tabulation ************************************
 public static int zok(int[] profit, int[] weights, int cap) {
 
     int n = profit.length;
